c81b3f32349b11119378ccad8a5df678f0d0b4ba
navinc <navinc@me.com>
2022-04-19 14:16:50 -0700

    Updates

---
+++ precache-manifest.65b839347ada3acf4c02af1d7c094e90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "372973c7db2ed33d2c012615a22eae07",
+    "url": "http://localhost/public/index.html"
+  },
+  {
+    "revision": "2f4e54c404069b2ed7e5",
+    "url": "http://localhost/public/static/css/main.50036214.chunk.css"
+  },
+  {
+    "revision": "8a32d8f7c4027656d47e",
+    "url": "http://localhost/public/static/js/2.491f7f76.chunk.js"
+  },
+  {
+    "revision": "fad494da9b4640823737c4723b8e88cc",
+    "url": "http://localhost/public/static/js/2.491f7f76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f4e54c404069b2ed7e5",
+    "url": "http://localhost/public/static/js/main.04cdd1af.chunk.js"
+  },
+  {
+    "revision": "118010838044038f4c00",
+    "url": "http://localhost/public/static/js/runtime-main.189377ca.js"
+  },
+  {
+    "revision": "187485bbe8f2b78932cb10dff1aec8ff",
+    "url": "http://localhost/public/static/media/App.187485bb.less"
+  },
+  {
+    "revision": "49174c736a9c871baaeb2db6d5372eee",
+    "url": "http://localhost/public/static/media/logo.49174c73.svg"
+  }
+]);
